dcdfc4583f4e8293cce848bb63eaeaac43976f1e
Ivo Nascimento <iannsp@gmail.com>
2011-08-30 00:26:08 +0000

    move to root dir
    git-svn-id: http://svn.php.net/repository/pear/packages/Cache/branches/Cache_1_5_7@315750 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ tests/Cache_ContainerTest.php
@@ -0,0 +1,259 @@
+<?php
+
+require_once 'Cache.php';
+
+require_once 'PHPUnit/Framework/TestCase.php';
+
+/**
+ * Cache_Container test case.
+ */
+class Cache_ContainerTest extends PHPUnit_Framework_TestCase {
+
+    /**
+     * @var Cache_Container
+     */
+    private $Cache_Container;
+
+    /**
+     * Prepares the environment before running a test.
+     */
+    protected function setUp() {
+        parent::setUp ();
+
+        // TODO Auto-generated Cache_ContainerTest::setUp()
+
+
+        $this->Cache_Container = new Cache_Container(/* parameters */);
+
+    }
+
+    /**
+     * Cleans up the environment after running a test.
+     */
+    protected function tearDown() {
+        // TODO Auto-generated Cache_ContainerTest::tearDown()
+
+
+        $this->Cache_Container = null;
+
+        parent::tearDown ();
+    }
+
+    /**
+     * Constructs the test case.
+     */
+    public function __construct() {
+        // TODO Auto-generated constructor
+    }
+
+    /**
+     * Tests Cache_Container->load()
+     */
+    public function testLoad() {
+        // TODO Auto-generated Cache_ContainerTest->testLoad()
+        $this->markTestIncomplete ( "load test not implemented" );
+
+        $this->Cache_Container->load(/* parameters */);
+
+    }
+
+    /**
+     * Tests Cache_Container->getUserdata()
+     */
+    public function testGetUserdata() {
+        // TODO Auto-generated Cache_ContainerTest->testGetUserdata()
+        $this->markTestIncomplete ( "getUserdata test not implemented" );
+
+        $this->Cache_Container->getUserdata(/* parameters */);
+
+    }
+
+    /**
+     * Tests Cache_Container->isExpired()
+     */
+    public function testIsExpired() {
+        // TODO Auto-generated Cache_ContainerTest->testIsExpired()
+        $this->markTestIncomplete ( "isExpired test not implemented" );
+
+        $this->Cache_Container->isExpired(/* parameters */);
+
+    }
+
+    /**
+     * Tests Cache_Container->isCached()
+     */
+    public function testIsCached() {
+        // TODO Auto-generated Cache_ContainerTest->testIsCached()
+        $this->markTestIncomplete ( "isCached test not implemented" );
+
+        $this->Cache_Container->isCached(/* parameters */);
+
+    }
+
+    /**
+     * Tests Cache_Container->fetch()
+     */
+    public function testFetch() {
+        // TODO Auto-generated Cache_ContainerTest->testFetch()
+        $this->markTestIncomplete ( "fetch test not implemented" );
+
+        $this->Cache_Container->fetch(/* parameters */);
+
+    }
+
+    /**
+     * Tests Cache_Container->save()
+     */
+    public function testSave() {
+        // TODO Auto-generated Cache_ContainerTest->testSave()
+        $this->markTestIncomplete ( "save test not implemented" );
+
+        $this->Cache_Container->save(/* parameters */);
+
+    }
+
+    /**
+     * Tests Cache_Container->remove()
+     */
+    public function testRemove() {
+        // TODO Auto-generated Cache_ContainerTest->testRemove()
+        $this->markTestIncomplete ( "remove test not implemented" );
+
+        $this->Cache_Container->remove(/* parameters */);
+
+    }
+
+    /**
+     * Tests Cache_Container->flush()
+     */
+    public function testFlush() {
+        // TODO Auto-generated Cache_ContainerTest->testFlush()
+        $this->markTestIncomplete ( "flush test not implemented" );
+
+        $this->Cache_Container->flush(/* parameters */);
+
+    }
+
+    /**
+     * Tests Cache_Container->idExists()
+     */
+    public function testIdExists() {
+        // TODO Auto-generated Cache_ContainerTest->testIdExists()
+        $this->markTestIncomplete ( "idExists test not implemented" );
+
+        $this->Cache_Container->idExists(/* parameters */);
+
+    }
+
+    /**
+     * Tests Cache_Container->garbageCollection()
+     */
+    public function testGarbageCollection() {
+        // TODO Auto-generated Cache_ContainerTest->testGarbageCollection()
+        $this->markTestIncomplete ( "garbageCollection test not implemented" );
+
+        $this->Cache_Container->garbageCollection(/* parameters */);
+
+    }
+
+    /**
+     * Tests Cache_Container->preload()
+     */
+    public function testPreload() {
+        // TODO Auto-generated Cache_ContainerTest->testPreload()
+        $this->markTestIncomplete ( "preload test not implemented" );
+
+        $this->Cache_Container->preload(/* parameters */);
+
+    }
+
+    /**
+     * Tests Cache_Container->flushPreload()
+     */
+    public function testFlushPreload() {
+        // TODO Auto-generated Cache_ContainerTest->testFlushPreload()
+        $this->markTestIncomplete ( "flushPreload test not implemented" );
+
+        $this->Cache_Container->flushPreload(/* parameters */);
+
+    }
+
+    /**
+     * Tests Cache_Container->setOptions()
+     */
+    public function testSetOptions() {
+        // TODO Auto-generated Cache_ContainerTest->testSetOptions()
+        $this->markTestIncomplete ( "setOptions test not implemented" );
+
+        $this->Cache_Container->setOptions(/* parameters */);
+
+    }
+
+    /**
+     * Tests Cache_Container->setAllowedOptions()
+     */
+    public function testSetAllowedOptions() {
+        // TODO Auto-generated Cache_ContainerTest->testSetAllowedOptions()
+        $this->markTestIncomplete ( "setAllowedOptions test not implemented" );
+
+        $this->Cache_Container->setAllowedOptions(/* parameters */);
+
+    }
+
+    /**
+     * Tests Cache_Container->hasBeenSet()
+     */
+    public function testHasBeenSet() {
+        // TODO Auto-generated Cache_ContainerTest->testHasBeenSet()
+        $this->markTestIncomplete ( "hasBeenSet test not implemented" );
+
+        $this->Cache_Container->hasBeenSet(/* parameters */);
+
+    }
+
+    /**
+     * Tests Cache_Container->getAllowedOptions()
+     */
+    public function testGetAllowedOptions() {
+        // TODO Auto-generated Cache_ContainerTest->testGetAllowedOptions()
+        $this->markTestIncomplete ( "getAllowedOptions test not implemented" );
+
+        $this->Cache_Container->getAllowedOptions(/* parameters */);
+
+    }
+
+    /**
+     * Tests Cache_Container->encode()
+     */
+    public function testEncode() {
+        // TODO Auto-generated Cache_ContainerTest->testEncode()
+        $this->markTestIncomplete ( "encode test not implemented" );
+
+        $this->Cache_Container->encode(/* parameters */);
+
+    }
+
+    /**
+     * Tests Cache_Container->decode()
+     */
+    public function testDecode() {
+        // TODO Auto-generated Cache_ContainerTest->testDecode()
+        $this->markTestIncomplete ( "decode test not implemented" );
+
+        $this->Cache_Container->decode(/* parameters */);
+
+    }
+
+    /**
+     * Tests Cache_Container->getExpiresAbsolute()
+     */
+    public function testGetExpiresAbsolute() {
+        // TODO Auto-generated Cache_ContainerTest->testGetExpiresAbsolute()
+        $this->markTestIncomplete ( "getExpiresAbsolute test not implemented" );
+
+        $this->Cache_Container->getExpiresAbsolute(/* parameters */);
+
+    }
+
+}
+
